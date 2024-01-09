8aba01f71f81dd94f4a261c1ecfd03420bb8c87c
ArnauBadenas <4bvives@gmail.com>
2024-01-09 19:07:25 +0100

    pinia test branches

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from "./stores/productStore";
 import { useCartStore } from "./stores/cartStore";
-
+//TEST AQUESTA NO ES LA PRACTICA 1
 /*
 Si no volem accedir a tot l’store, podem extreure un part d’aquesta,
 però per assegurar que l’estat es manté reactiu hem de fer servir la funció storeToRefs
